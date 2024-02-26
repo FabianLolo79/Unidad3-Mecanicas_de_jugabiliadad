2dba91dbc708f68e5fdae6f6798fa6884f741f5f
Fabián E. García <fabianegl79@gmail.com>
2024-02-26 13:39:23 -0300

    generar oleadas de enemigos, destruirlos al caer y volver a generar enemigos

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,20 +7,32 @@ public class SpawnManager : MonoBehaviour
     public GameObject enemyPrefab;
     private float spawnRange = 9;
 
+    public int enemyCount;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if (enemyCount == 0)
+        {
+            SpawnEnemyWave(1);
+        }
     }
 
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn ; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
     private Vector3 GenerateSpawnPosition()
     {
         float spawnPositionX = Random.Range(-spawnRange, spawnRange);
